f73008c2311cfa7786311650f3cfc1b0816f6c33
BILL ZHEDRICK GASPAR <88978837+itsmeNEK@users.noreply.github.com>
2023-07-02 13:10:37 +0800

    production and added present to service record

--- app/Http/Controllers/users/pds/Personal.php
+++ app/Http/Controllers/users/pds/Personal.php
@@ -53,7 +53,9 @@ class Personal extends Controller
         $this->personal->middle_name = strtoupper($request->middle_name);
         $this->personal->ext_name = strtoupper($request->ext_name);
         $this->personal->salutation_before =$request->salutation_before;
-        $this->personal->salutation_after =', ' . $request->salutation_after;
+        if($request->salutation_after) {
+            $this->personal->salutation_after =', ' . $request->salutation_after;
+        }
         $this->personal->date_birth = $request->date_birth;
         $this->personal->place_birth = strtoupper($request->place_birth);
         $this->personal->sex = $request->sex;
@@ -133,7 +135,9 @@ class Personal extends Controller
         $personal->middle_name = $request->middle_name;
         $personal->ext_name = $request->ext_name;
         $personal->salutation_before = $request->salutation_before;
-        $personal->salutation_after = ', ' . $request->salutation_after;
+        if($request->salutation_after) {
+            $personal->salutation_after =', ' . $request->salutation_after;
+        }
         $personal->date_birth = $request->date_birth;
         $personal->place_birth = $request->place_birth;
         $personal->sex = $request->sex;
