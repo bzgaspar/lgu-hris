e0949b03ef9f3806a0bb084bc0efc14fd20a2dd4
BILL ZHEDRICK GASPAR <88978837+itsmeNEK@users.noreply.github.com>
2023-07-15 07:55:38 +0800

    prod 26

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => '/usr/bin/mysql/', // only the path, so without `mysqldump` or `pg_dump`
+                'dump_binary_path' => '/usr/bin/', // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 * 15, // 5 minute timeout
              ],
