64420c8bee5b7197e02379d1674d8d0fe7ab4637
BILL ZHEDRICK GASPAR <88978837+itsmeNEK@users.noreply.github.com>
2023-07-15 08:02:00 +0800

    prod 26 with backup and clean

--- app/Http/Controllers/users/User.php
+++ app/Http/Controllers/users/User.php
@@ -124,10 +124,10 @@ class User extends Controller
     {
         Artisan::call('backup:run --only-db');
         $path = storage_path('app\HRIS---LGU-Delfin-Albano\*');
+        dd($path);
         $latest_ctime = 0;
         $latest_filename = '';
         $files = glob($path);
-        dd(Artisan::output());
         foreach ($files as $file) {
             if (is_file($file) && filectime($file) > $latest_ctime) {
                 $latest_ctime = filectime($file);
